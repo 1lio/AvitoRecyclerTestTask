0c471b4024643598fcb165517ec91baeaf2b0cd3
ViacheslavSukhov <vi.sukhov@ya.ru>
2021-01-17 03:53:43 +0300

    Used recycler as the root view

--- app/src/main/kotlin/ru/avito/recycler/view/MainActivity.kt
+++ app/src/main/kotlin/ru/avito/recycler/view/MainActivity.kt
@@ -1,13 +1,23 @@
 package ru.avito.recycler.view
 
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity: AppCompatActivity() {
+/** Я не доволен своим решением. С одной стороны можно было использовать viewModel и было бы по красоте,
+ *  но мне не хочется тащить лишние либы. Поэтому тут типа MVC. Еще есть пара леких косыков (статики).
+ *
+ *  Мои контакты
+ *
+ *  Telegram: @sukhovvv
+ *  E-mail: svoefremov@ya.ru
+ *  Вячеслав
+ *
+ *  */
+
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(View(this))
+        setContentView(MainRecyclerView(context = this))
     }
 }
