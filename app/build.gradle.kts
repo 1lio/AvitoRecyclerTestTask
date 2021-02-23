dfb8dc9fa078d1b43eef6d77cc1eba1e5ab5c0fd
Viacheslav Sukhov <svoefremov@yandex.ru>
2021-02-23 12:35:19 +0300

    Added activity-ktx

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,13 +53,15 @@ dependencies {
 
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.3.0")
 
+    implementation("androidx.activity:activity-ktx:1.2.0")
+
     // DI: Dagger Hilt
     val hiltVer = rootProject.extra.get("hiltVer")
     implementation("com.google.dagger:hilt-android:$hiltVer")
     kapt("com.google.dagger:hilt-android-compiler:$hiltVer")
 
     // Tests
-    testImplementation("junit:junit:4.13.1")
+    testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.2")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
 }
